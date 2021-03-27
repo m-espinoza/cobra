dd4f16b78a9eee29122bf3eab6cc41241714700d
Espi <espinoza.manuel1989@gmail.com>
2021-03-27 20:48:19 -0300

    Django tutorial: 	- Tutorial 1 - Crear proyecto - Terminada 	- Tutorial 2 - Primera aplicación Parte 1 - Terminada 	- Tutorial 3 - Primera aplicación parte 2 - En proceso - https://docs.djangoproject.com/es/3.1/intro/tutorial03/#removing-hardcoded-urls-in-templates

--- public/mysite/mysite/settings.py
+++ public/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'm17190',
+        'HOST': 'moradb'
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-AR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
