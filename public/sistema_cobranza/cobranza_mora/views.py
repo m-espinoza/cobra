b1b60bbc2dca6fdf322923ccae72ad91467fb4ed
Espi <espinoza.manuel1989@gmail.com>
2021-04-18 13:16:29 -0300

    Filtrado por lista:
    	- Filtro lista en las que solo el usuario tiene clientes asignados
    	- Filtro que el cliente esté activo

--- public/sistema_cobranza/cobranza_mora/views.py
+++ public/sistema_cobranza/cobranza_mora/views.py
@@ -4,26 +4,30 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models import Count
 
 from datetime import date  
 
 from .models import Lista, Lista_cliente, Cliente, Empresa
 
-#def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
 
 class IndexView(generic.ListView):
     template_name = 'cobranza_mora/index.html'
     context_object_name = 'latest_list'
 
     def get_queryset(self):
-        """Busco las listas que estan activas"""
+        """
+        Busco las listas que estan activas y 
+        los clientes que solo el usuario logueado puede ver
+        
+        """
+
         return Lista.objects.filter(
-            fecha_inicio__lte = date.today(),
-            fecha_vencimiento__gte = date.today()
-            ).exclude(
-                estado_id=2
-                )
+                fecha_inicio__lte = date.today(),
+                fecha_vencimiento__gte = date.today(),
+                lista_cliente__estado_id = 1,
+                lista_cliente__user_id = self.request.user.id
+            ).order_by('fecha_creado').distinct()
 
 class DetailView(generic.DetailView):
     model = Lista
