ba2d94d6a3a397d505cd2b80cae3b8a2eea34aa4
Espi <espinoza.manuel1989@gmail.com>
2022-07-27 00:18:41 -0300

    Mejora en redireccion de login

--- public/sistema_cobranza/cobranza_mora/urls.py
+++ public/sistema_cobranza/cobranza_mora/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 	path('', LoginView.as_view(template_name='cobranza_mora/login.html'), name='login'),
 	path('logout/', LogoutView.as_view(template_name='cobranza_mora/logout.html'), name='logout'),
 	path('evento/', views.evento_crear_view, name='evento_crear'),
-	path('listas_detalle/<int:id_lista>', views.lista_dinamica_view, name='lista_dinamica'),
+	path('listas_detalle/<int:id_lista>', views.lista_dinamica_view, name='listas_detalle'),
 	path('cliente_detalle/', views.cliente_detalle_view, name='cliente_detalle'),
 ]
